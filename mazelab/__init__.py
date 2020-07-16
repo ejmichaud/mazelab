9aa8a044bfe1869e4748fd8cb13950403c013fbe
Eric Michaud <eric.michaud99@gmail.com>
2020-07-16 11:53:13 -0700

    added simple maze environments to auto-add to registry on import

--- mazelab/__init__.py
+++ mazelab/__init__.py
@@ -1,10 +1,90 @@
+import numpy as np
+import gym
+
 from .color_style import DeepMindColor
-
 from .object import Object
-
 from .motion import VonNeumannMotion
 from .motion import MooreMotion
-
 from .maze import BaseMaze
+from .env import BaseEnv, Maze, Env
 
-from .env import BaseEnv
+from .generators import random_maze
+
+env_id = "EmptyMaze-10x10-FixedGoal-v0"
+x = random_maze(width=10, height=10, complexity=0.7, density=0.0)
+EmptyMaze_10x10_FixedGoal_v0 = Env(Maze(x),
+          randomize_start=True, 
+          randomize_goal=False)
+EmptyMaze_10x10_FixedGoal_v0_entry_point = lambda: EmptyMaze_10x10_FixedGoal_v0 
+gym.envs.register(id=env_id, entry_point=EmptyMaze_10x10_FixedGoal_v0_entry_point, max_episode_steps=200)
+
+
+env_id = "EmptyMaze-10x10-RandomGoal-v0"
+x = random_maze(width=10, height=10, complexity=0.7, density=0.0)
+EmptyMaze_10x10_RandomGoal_v0 = Env(Maze(x),
+          randomize_start=True, 
+          randomize_goal=True)
+EmptyMaze_10x10_RandomGoal_v0_entry_point = lambda: EmptyMaze_10x10_RandomGoal_v0
+gym.envs.register(id=env_id, entry_point=EmptyMaze_10x10_RandomGoal_v0_entry_point, max_episode_steps=200)
+
+
+x10 = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+       [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+       [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+       [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
+       [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+       [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
+
+env_id = "Maze-10x10-FixedGoal-v0"
+Maze_10x10_FixedGoal_v0 = Env(Maze(x10),
+          randomize_start=True, 
+          randomize_goal=False)
+Maze_10x10_FixedGoal_v0_entry_point = lambda: Maze_10x10_FixedGoal_v0
+gym.envs.register(id=env_id, entry_point=Maze_10x10_FixedGoal_v0_entry_point, max_episode_steps=200)
+
+
+env_id = "Maze-10x10-RandomGoal-v0"
+Maze_10x10_RandomGoal_v0 = Env(Maze(x10),
+          randomize_start=True, 
+          randomize_goal=True)
+Maze_10x10_RandomGoal_v0_entry_point = lambda: Maze_10x10_RandomGoal_v0
+gym.envs.register(id=env_id, entry_point=Maze_10x10_RandomGoal_v0_entry_point, max_episode_steps=200)
+
+
+x15 = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+       [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+       [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+       [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+       [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+       [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+       [1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+       [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+       [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+       [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+       [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
+
+env_id = "Maze-15x15-FixedGoal-v0"
+Maze_15x15_FixedGoal_v0 = Env(Maze(x15),
+          randomize_start=True, 
+          randomize_goal=False)
+Maze_15x15_FixedGoal_v0_entry_point = lambda: Maze_15x15_FixedGoal_v0
+gym.envs.register(id=env_id, entry_point=Maze_15x15_FixedGoal_v0_entry_point, max_episode_steps=200)
+
+
+env_id = "Maze-15x15-RandomGoal-v0"
+Maze_15x15_RandomGoal_v0 = Env(Maze(x15),
+          randomize_start=True, 
+          randomize_goal=True)
+Maze_15x15_RandomGoal_v0_entry_point = lambda: Maze_15x15_RandomGoal_v0
+gym.envs.register(id=env_id, entry_point=Maze_15x15_RandomGoal_v0_entry_point, max_episode_steps=200)
+
+
--- mazelab/env.py
+++ mazelab/env.py
@@ -1,10 +1,24 @@
 from abc import ABC
 from abc import abstractmethod
 
+import random
+
 import numpy as np
 import gym
 from gym.utils import seeding
+from gym.spaces import Box
+from gym.spaces import Discrete
 from PIL import Image
+
+from mazelab import BaseMaze
+from mazelab import Object
+from mazelab import DeepMindColor as color
+from mazelab import VonNeumannMotion
+
+# GAMMA = 0.999
+
+# def manhattan_distance(x, y):
+#     return np.abs(x[0]-y[0]) + np.abs(x[1]-y[1])
 
 
 class BaseEnv(gym.Env, ABC):
@@ -32,7 +46,7 @@ class BaseEnv(gym.Env, ABC):
     def get_image(self):
         pass
     
-    def render(self, mode='human', max_width=500):
+    def render(self, mode='human', max_width=300):
         img = self.get_image()
         img = np.asarray(img).astype(np.uint8)
         img_height, img_width = img.shape[:2]
@@ -53,3 +67,115 @@ class BaseEnv(gym.Env, ABC):
         if self.viewer is not None:
             self.viewer.close()
             self.viewer = None
+
+
+class Maze(BaseMaze):
+    def __init__(self, maze):
+        self.x = maze
+        super().__init__()
+    
+    @property
+    def size(self):
+        return self.x.shape
+    
+    def make_objects(self):
+        free = Object('free', 0, color.free, False, np.stack(np.where(self.x == 0), axis=1))
+        obstacle = Object('obstacle', 1, color.obstacle, True, np.stack(np.where(self.x == 1), axis=1))
+        agent = Object('agent', 2, color.agent, False, [])
+        goal = Object('goal', 3, color.goal, False, [])
+        return free, obstacle, agent, goal
+
+
+class Env(BaseEnv):
+    def __init__(self, maze, start_pos=None, 
+                             goal_pos=None, 
+                             randomize_start=False, 
+                             randomize_goal=False,
+                             # shaping=False):
+                             ):
+        """
+        A maze gym environment based on `maze`.
+        
+        Args:
+            maze (Maze): A maze object with "free", "obstacle", "agent", and "goal" objects.
+            start_pos (tuple): If `randomize_start` is False, the agent starting position.
+            goal_pos (tuple): If `randomize_goal` is False, the goal position.
+            randomize_start (bool): Whether to start the agent in a random free space on reset.
+            randomize_goal (bool): Whether to randomize the goal position between episodes.
+        """
+        super().__init__()
+        self.maze = maze
+        self.motions = VonNeumannMotion()
+        w, h = self.maze.size
+        if start_pos is None: self.start_pos = [1, 1]
+        else:                 self.start_pos = start_pos
+        if goal_pos is None:  self.goal_pos = [w-2, h-2]
+        else:                 self.goal_pos = goal_pos
+        self.randomize_start = randomize_start
+        self.randomize_goal = randomize_goal
+        # self.shaping = shaping
+        self.observation_space = Box(low=0, high=len(self.maze.objects), shape=self.maze.size, dtype=np.uint8)
+        self.action_space = Discrete(len(self.motions))
+        self.reset()
+        
+    def step(self, action):
+        motion = self.motions[action]
+        current_position = self.maze.objects.agent.positions[0]
+        new_position = [current_position[0] + motion[0], current_position[1] + motion[1]]
+        valid = self._is_valid(new_position)
+        if valid:
+            self.maze.objects.agent.positions = [new_position]
+        if self._is_goal(new_position):
+            reward = +10
+            done = True
+        elif not valid:
+            reward = -0.1
+            done = False
+        else:
+            reward = -0.01
+            done = False
+        # d_s0 = manhattan_distance(current_position, self.maze.objects.goal.positions[0])
+        # d_s1 = manhattan_distance(new_position, self.maze.objects.goal.positions[0])
+        # shaping = GAMMA*(-d_s1) - (-d_s0)
+        # shaping = shaping if self.shaping else 0
+        # return self.maze.to_value(), reward + shaping, done, {}
+        return self.maze.to_value(), reward, done, {}
+        
+    def reset(self):
+        if self.randomize_start:
+            self.maze.objects.agent.positions = [list(random.choice(self.maze.objects.free.positions))]
+        else:
+            self.maze.objects.agent.positions = [self.start_pos]
+        if self.randomize_goal: 
+            self.maze.objects.goal.positions = [list(random.choice(self.maze.objects.free.positions))]
+        else:
+            self.maze.objects.goal.positions = [self.goal_pos] 
+        return self.maze.to_value()
+    
+    def _is_valid(self, position):
+        nonnegative = position[0] >= 0 and position[1] >= 0
+        within_edge = position[0] < self.maze.size[0] and position[1] < self.maze.size[1]
+        passable = not self.maze.to_impassable()[position[0]][position[1]]
+        return nonnegative and within_edge and passable
+    
+    def _is_goal(self, position):
+        out = False
+        for pos in self.maze.objects.goal.positions:
+            if position[0] == pos[0] and position[1] == pos[1]:
+                out = True
+                break
+        return out
+    
+    def get_image(self):
+        return self.maze.to_rgb()
+
+
+
+
+
+
+
+
+
+
+
